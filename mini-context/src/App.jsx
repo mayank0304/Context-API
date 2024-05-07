882d85383d61ada0878e31f518f15f5d48bd9579
mayank0304 <mayank771768@gmail.com>
2024-05-07 08:21:16 +0530

    React App created using vite

---
+++ mini-context/src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  
+  return (
+    <>
+    <div className="bg-gray-600 h-screen py-8">
+      <h1 className=" bg-black text-center text-white text-3xl max-w-6xl mx-auto rounded-full p-4" >
+      Hello This is Mayank, learning context-api!
+    </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
